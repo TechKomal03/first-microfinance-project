a6dc2b3d73255c73a88c5cefe91acb079aecfec1
TechKomal03 <komaltara2003@gmail.com>
2024-10-23 13:20:56 +0530

    First finnace project

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
